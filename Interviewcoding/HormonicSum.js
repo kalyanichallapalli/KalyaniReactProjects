d072d42d248f9556b0e09abd341cbfe4016d2b60
kalyani <kalyanireddy9@gmail.com>
2022-04-14 15:20:50 -0700

    hormonic sum

---
+++ Interviewcoding/HormonicSum.js
@@ -0,0 +1,28 @@
+function sum(n){
+
+    let s = 0.0;
+    for(let i = 1; i<=n; i++){
+        if(n<2){
+            return 1
+        }else{
+            s=s+1/i;
+        }
+     }         
+     return s;
+
+}
+console.log(sum(5))
+function sum(n){
+   if(n<2){
+       return 1
+   }else{
+       return 1/n+(sum(n-1))
+   }
+
+}
+console.log(sum(1))
+console.log(sum(5))
+console.log(sum(8))
+console.log(sum(10))
+
+
