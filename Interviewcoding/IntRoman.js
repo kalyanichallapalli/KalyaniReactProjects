d87409ef1f72de6231297f71ecd7f471f449859e
kalyani <kalyanireddy9@gmail.com>
2022-04-14 10:10:29 -0700

    new

---
+++ Interviewcoding/IntRoman.js
@@ -0,0 +1,53 @@
+const intToRoman = (num) => {
+    const map = {M:1000,D:500,C:100,L:50,X:10,V:5,I:1}
+    let result = '';
+    for (key in map) {
+      result += key.repeat(Math.floor(num / map[key]));
+      num %= map[key];
+    }
+    
+    return result;
+  };
+console.log(intToRoman(2790))  
+
+function roman(num) {
+    var map = {M:1000,D:500,C:100,L:50,X:10,V:5,I:1}
+    let roman = '';
+    for ( key in map ) {
+      while ( num >= map[key] ) {
+        roman += key;
+        num = num-map[key];
+      }
+    }
+    return roman;
+  }
+  console.log(roman(2970))
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
