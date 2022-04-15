a53c54ab95f778358a3aaf4041296c7f3d597dc7
kalyani <kalyanireddy9@gmail.com>
2022-04-15 11:24:11 -0700

    nw

--- Interviewcoding/DeepCopy.js
+++ Interviewcoding/DeepCopy.js
@@ -6,8 +6,8 @@ let orgvab = {
 }
 let newvab = orgvab;
 newvab.name = 'Kalyani Challapalli'
-console.log(orgvab)
-console.log(newvab)
+// console.log(orgvab)
+// console.log(newvab)
 
 let shvariable = {
     firstname: 'Kalyani',
@@ -27,5 +27,34 @@ newsh.age = 38
 newsh.address.city = 'SFO';
 newsh.address.house = 1234;
 
-console.log(shvariable)
-console.log(newsh)
+// console.log(shvariable)
+// console.log(newsh)
+//shallow
+let person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age:21
+    
+};
+
+let copiedPerson = person;
+// let copiedPerson = Object.assign({}, person);
+copiedPerson.firstName = 'kalyani'; // disconnected
+console.log(person),
+console.log(copiedPerson);
+
+//deep
+let person1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age:24
+};
+
+
+let NP = JSON.parse(JSON.stringify(person1));
+
+NP.firstName = 'Jane'; // disconnected
+NP.age = 21;
+console.log(person1);
+console.log(NP);
+
