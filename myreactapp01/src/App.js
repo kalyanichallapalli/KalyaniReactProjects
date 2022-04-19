30485f5446c985b45ce801204322187b6fee6e16
kalyani <kalyanireddy9@gmail.com>
2022-04-19 14:09:07 -0700

    Ref chnages

--- myreactapp01/src/App.js
+++ myreactapp01/src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
-import Button from './TwoComponents/Button';
-import Calculator from './CalC/Calculator'
-import{BrowserRouter,Route, Routes} from "react-router-dom";
-import NavBar from './Reactrouter/NavBar';
-import Home from './Reactrouter/Home';
-import Dashbord from './Reactrouter/Dashbord';
-import About from './Reactrouter/About';
-import Control from './ContronandUncontrolled/Control';
-import Uncontrol from './ContronandUncontrolled/Uncontrol';
-import NornalComp from './HOC/NornalComp';
-import UseEffect from './Hooks/UseEffect';
-import UseState from './Hooks/useState';
-
+// import Button from './TwoComponents/Button';
+// import Calculator from './CalC/Calculator'
+// import{BrowserRouter,Route, Routes} from "react-router-dom";
+// import NavBar from './Reactrouter/NavBar';
+// import Home from './Reactrouter/Home';
+// import Dashbord from './Reactrouter/Dashbord';
+// import About from './Reactrouter/About';
+// import Control from './ContronandUncontrolled/Control';
+// import Uncontrol from './ContronandUncontrolled/Uncontrol';
+// import NornalComp from './HOC/NornalComp';
+// import UseEffect from './Hooks/UseEffect';
+// import UseState from './Hooks/useState';
+import InputRef from './UseRef/InputRef '
 function App() {
   return (
     <div className="App">
-        {/* <Calculator></Calculator> */}
       {/* <BrowserRouter>
       <NavBar/> 
       <Routes>
@@ -29,8 +28,7 @@ function App() {
       {/* <NornalComp></NornalComp> */}
       {/* <UseState></UseState> */}
       {/* <UseEffect></UseEffect> */}
-      <Calculator>
-      </Calculator>
+      <InputRef></InputRef>
     </div>
   );
 }
