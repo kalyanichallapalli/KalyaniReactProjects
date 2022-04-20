0d9b993b8de3b05cca4cd41862c2ba52ce554c9b
kalyani <kalyanireddy9@gmail.com>
2022-04-20 12:31:21 -0700

    new

--- INT/HormonicSum.js
+++ INT/HormonicSum.js
@@ -1,26 +1,21 @@
 //HORMONIC COUNT
 function sum(n){
-
     let s = 0.0;
     for(let i = 1; i<=n; i++){
         if(n<2){
             return 1
         }else{
             s=s+1/i;
-        }
-     }         
+        }}         
      return s;
-
-}
+    }
 console.log(sum(5))
 function sum(n){
    if(n<2){
        return 1
    }else{
        return 1/n+(sum(n-1))
-   }
-
-}
+   }}
 console.log(sum(1))
 console.log(sum(5))
 console.log(sum(8))
--- INT/Palindrom.js
+++ INT/Palindrom.js
@@ -1,34 +1,23 @@
 function palindrom(str){       
  let newstr = str.split('').reverse().join('') //if its string
- console.log(newstr)
         if(str == newstr){
             return 'palindrome';
         } else {
-            return 'not palindrome'
-        }
-}
-// console.log(palindrom('aba'))
+            return 'not palindrome'}}
 function Pdrom(num){  
-    console.log('Input:', num)
     let newnum = num.toString().split('').reverse().join('') // if its intiger
-    console.log('num.toString:',newnum)
     if(num == newnum){
         return 'palindrome';
     } else {
-        return 'not palindrome'
-    }
-}
-// console.log(Pdrom(121))
-function drom(num){  
+        return 'not palindrome'}}
+function drom(num){   // if its array
     console.log('Input:', num)
     let len = num.length;
     for(let i=0; i<len; i++)
       if(num[i] == num[len - 1 -i]) {
         return 'palindrome';
     } else {
-        return 'not palindrome'
-    }
-}
+        return 'not palindrome' }}
 console.log(drom([1,2,3]))
 console.log(drom([1,2,1]))
 
