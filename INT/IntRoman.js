ae35a6ceddf8dba6dd04e8108a09df25a9a3a680
kalyani <kalyanireddy9@gmail.com>
2022-04-21 14:43:19 -0700

    new

--- INT/IntRoman.js
+++ INT/IntRoman.js
@@ -8,13 +8,12 @@ const intToRoman = (num) => {
     
     return result;
   };
-console.log(intToRoman(2790))  
+console.log(intToRoman(2970))  
 
 function roman(num) {
     let map = {M:1000,D:500,C:100,L:50,X:10,V:5,I:1}
     let roman = '';
     for ( key in map ) {
-      console.log(map[key])
       while ( num >= map[key] ) {
         roman += key;
         num = num-map[key];
