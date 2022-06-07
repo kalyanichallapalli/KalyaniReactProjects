4011c85beffee06a0327268903dad0aa5b229424
kalyani <kalyanireddy9@gmail.com>
2022-06-07 13:03:51 -0700

    app

--- myreactapp01/src/App.js
+++ myreactapp01/src/App.js
@@ -4,6 +4,7 @@ import Reducer from './Redux/Reducer';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import NavBar from './Reactrouter/NavBar';
+import { BrowserRouter,router,route } from 'react-router-dom';
 const store = createStore(Reducer)
 function App() {
   return (
